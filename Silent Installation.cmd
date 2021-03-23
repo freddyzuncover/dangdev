3618ebf975c2733dc6012980f8db3533ad5ec989
DangDev <dream858vip@gmail.com>
2021-03-23 19:45:37 +0700

    Add files via upload

---
+++ Silent Installation.cmd
@@ -0,0 +1,24 @@
+@ECHO OFF
+Color 0B
+@cls
+echo.
+echo.
+echo.
+@echo    //////////////////////////////////////////////////////////////
+@echo    /                                                            /
+@echo    /           Avira Phantom VPN Pro is installing...           /
+@echo    /                                                            /
+@echo    /                       Please wait...                       /
+@echo    /                                                            /
+@echo    /                   Dont close this window                   /
+@echo    /                                                            /
+@echo    /                     After Installation                     /
+@echo    /                  Dont forget to Subscribe                  /
+@echo    /                        TECH MORON                          /
+@echo    /                      youtube channel                       /
+@echo    /               for more awesome video tutorials             /
+@echo    /                                                            /
+@echo    //////////////////////////////////////////////////////////////
+@echo off
+FOR %%i IN ("Avira Phantom VPN*.exe") DO Set FileName="%%i"
+%FileName% /VERYSILENT
