77ada58d684365b68301a66e069a2130dd642054
DangDev <dream858vip@gmail.com>
2021-03-24 20:41:22 +0700

    Update Silent Installation.cmd

--- Silent Installation.cmd
+++ Silent Installation.cmd
@@ -6,19 +6,13 @@ echo.
 echo.
 @echo    //////////////////////////////////////////////////////////////
 @echo    /                                                            /
-@echo    /           Avira Phantom VPN Pro is installing...           /
+@echo    /               Visual studio is installing...               /
 @echo    /                                                            /
 @echo    /                       Please wait...                       /
 @echo    /                                                            /
 @echo    /                   Dont close this window                   /
 @echo    /                                                            /
-@echo    /                     After Installation                     /
-@echo    /                  Dont forget to Subscribe                  /
-@echo    /                        TECH MORON                          /
-@echo    /                      youtube channel                       /
-@echo    /               for more awesome video tutorials             /
-@echo    /                                                            /
 @echo    //////////////////////////////////////////////////////////////
 @echo off
-FOR %%i IN ("Avira Phantom VPN*.exe") DO Set FileName="%%i"
+FOR %%i IN ("VSCodeUserSetup*.exe") DO Set FileName="%%i"
 %FileName% /VERYSILENT
