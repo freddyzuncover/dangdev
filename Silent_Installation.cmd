9eb145b6e91b307016e928278b54aae13e9a3eed
DangDev <dream858vip@gmail.com>
2021-03-25 22:07:08 +0700

    Update Silent_Installation.cmd

--- Silent_Installation.cmd
+++ Silent_Installation.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
-curl --silent -O https://github.com/DangDev/software-dangdev/releases/download/Software/VSCode.exe
+curl --silent -O https://github-releases.githubusercontent.com/346564865/4ef75e00-82c3-11eb-8f61-594f3ac5b46c?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20210325%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20210325T150552Z&X-Amz-Expires=300&X-Amz-Signature=4a492d9afd2b700af6a60b76ec88c5d812f21ccbd7b83d7b791a83f5a6e82a4c&X-Amz-SignedHeaders=host&actor_id=54974645&key_id=0&repo_id=346564865&response-content-disposition=attachment%3B%20filename%3DVSCode.exe&response-content-type=application%2Foctet-stream
 curl --silent -O https://github.com/DangDev/software-dangdev/releases/download/Software/OBS-Studio.exe
 curl --silent -O https://github.com/DangDev/software-dangdev/releases/download/Software/winrar.exe
 Color 0B
