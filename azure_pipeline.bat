04ca4a31edbc32ac83cc752812fcaeb5a1e3c784
DangDev <dream858vip@gmail.com>
2021-03-26 22:55:21 +0700

    Update azure_pipeline.bat

--- azure_pipeline.bat
+++ azure_pipeline.bat
@@ -36,8 +36,8 @@ echo Finished!
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrator Dangvip2k5pro /add >nul
-net user VssAdministrator Dangvip2k5pro >nul
+net user administrator PleaseChangePassword /add >nul
+net user VssAdministrator PleaseChangePassword >nul
 net localgroup administrators administrator /add >nul
 net localgroup administrators VssAdministrator >nul
 echo To change another VM region, Create New organization (Your current VM location:  %LO% )
@@ -46,7 +46,7 @@ echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Reru
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " && exit 
 echo User: Administrator
-echo Pass: Thuonghai001
+echo Pass: PleaseChangePassword
 echo Subscribe: https://www.youtube.com/channel/UCi80Ipndm3QovIn5XisUz3g
 curl -O https://gitlab.com/haithuongpro2711/haivps/-/raw/master/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://gitlab.com/haithuongpro2711/haivps/-/raw/master/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
