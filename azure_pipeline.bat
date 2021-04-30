23994dff3c0b8b9b1650b4f5a511346354fe9d97
DangDev <dream858vip@gmail.com>
2021-04-30 11:37:56 +0700

    Update azure_pipeline.bat

--- azure_pipeline.bat
+++ azure_pipeline.bat
@@ -8,43 +8,28 @@ copy ngrok.exe C:\Windows\System32 >nul
 
 echo CONNECT NGROK AUTH TOKEN...
 start NGROK.bat >nul
-
-
-echo Check Region for NGROK...
-curl -s ifconfig.me >ip.txt
-set /p IP=<ip.txt
-curl -s "http://api.whoapi.com/?apikey=bde247b641898256b1aaefcd1df201d9&r=geo&domain=&ip=%IP%" >full.txt
-type full.txt | jq -r .geo_cc >region.txt
-type full.txt | jq -r .geo_country >location.txt
-set /p LO=<location.txt
-set /p RE=<region.txt
 start ngrok tcp --region ap 3389
 
 echo Setup Profile...
 tzutil /s "SE Asia Standard Time" >nul
-Reg Add "HKCU\SOFTWARE\Microsoft\Windows\DWM" /v ColorPrevalence /t REG_DWORD /d 1 /f >nul
-Reg Add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v ColorPrevalence /t REG_DWORD /d 1 /f >nul
 sc start audiosrv >nul
 
 echo Finished!
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
+net config server /srvcomment:"VanDang Windows desktop" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrator ThuongHai101 /add >nul
+net user administrator VanDang1 /add >nul
 net localgroup administrators administrator /add >nul
-echo To change another VM region, Create New organization (Your current VM location:  %LO% )
-echo Region Available: West Europe, Central US, East Asia, Brazil South, Canada Central, Autralia East, UK South, South India
-echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Rerun failed jobs to get a new RDP.
+net user VssAdministrator VanDang1 >nul
+net localgroup administrators VssAdministrator >nul
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " && exit 
 echo User: Administrator
-echo Pass: ThuongHai101
-curl -O https://gitlab.com/haithuongpro2711/haivps/-/raw/master/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
+echo Pass: [Secret]
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/DangDev/dangdev/main/software/FastConfigVPS_v2.3.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\EVKey Vietnamese Keyboard.exe" https://raw.githubusercontent.com/DangDev/dangdev/main/software/EVKey64.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Enable Session notification.vbs" https://raw.githubusercontent.com/DangDev/dangdev/main/Session.vbs > out.txt 2>&1
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
 
 sc config Audiosrv start= auto >nul
