3c1331afbdeb14e9a4126526b908af3426ad9697
DangDev <dream858vip@gmail.com>
2021-04-30 13:52:09 +0700

    Update azure_pipeline.bat

--- azure_pipeline.bat
+++ azure_pipeline.bat
@@ -8,6 +8,16 @@ copy ngrok.exe C:\Windows\System32 >nul
 
 echo CONNECT NGROK AUTH TOKEN...
 start NGROK.bat >nul
+
+
+echo Check Region for NGROK...
+curl -s ifconfig.me >ip.txt
+set /p IP=<ip.txt
+curl -s "http://api.whoapi.com/?apikey=bde247b641898256b1aaefcd1df201d9&r=geo&domain=&ip=%IP%" >full.txt
+type full.txt | jq -r .geo_cc >region.txt
+type full.txt | jq -r .geo_country >location.txt
+set /p LO=<location.txt
+set /p RE=<region.txt
 start ngrok tcp --region ap 3389
 
 echo Setup Profile...
@@ -19,7 +29,7 @@ sc start audiosrv >nul
 echo Finished!
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"VanDang Windows desktop" > out.txt 2>&1
+net config server /srvcomment:"VanDang" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 net user administrator VanDang1 /add >nul
 net localgroup administrators administrator /add >nul
@@ -34,6 +44,10 @@ curl -o "C:\Users\Public\Desktop\Enable Session notification.vbs" https://raw.gi
 diskperf -Y >nul
 
 sc config Audiosrv start= auto >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant VssAdministrator:F >nul
+ICACLS C:\Windows\Temp /grant VssAdministrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
 
 
 ping -n 99999999999999999999999999999999 10.10.10.10 >nul
