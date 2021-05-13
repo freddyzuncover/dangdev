f030a9a040c5c53e39a5c6ac93fdb2837f34a463
DangDev <dream858vip@gmail.com>
2021-05-13 19:55:22 +0700

    Update telebit.bat

--- telebit.bat
+++ telebit.bat
@@ -1,4 +1,39 @@
 title Telebit
+
+echo Setup Telebit
+npm install -g telebit
+
 echo Download all files...
 curl --silent -O https://raw.githubusercontent.com/DangDev/dangdev/main/telebit.zip
 tar -xf telebit.zip
+
+echo Setup Profile...
+tzutil /s "SE Asia Standard Time" >nul
+Reg Add "HKCU\SOFTWARE\Microsoft\Windows\DWM" /v ColorPrevalence /t REG_DWORD /d 1 /f >nul
+Reg Add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v ColorPrevalence /t REG_DWORD /d 1 /f >nul
+sc start audiosrv >nul
+
+echo Finished!
+@echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net config server /srvcomment:"VanDang" > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+net user administrator VanDang1 /add >nul
+net localgroup administrators administrator /add >nul
+net user VssAdministrator VanDang1 >nul
+echo User: Administrator
+echo Pass: [Secret]
+curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/DangDev/dangdev/main/software/FastConfigVPS_v2.3.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\EVKey Vietnamese Keyboard.exe" https://raw.githubusercontent.com/DangDev/dangdev/main/software/EVKey64.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Enable Session notification.vbs" https://raw.githubusercontent.com/DangDev/dangdev/main/Session.vbs > out.txt 2>&1
+diskperf -Y >nul
+
+sc config Audiosrv start= auto >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant VssAdministrator:F >nul
+ICACLS C:\Windows\Temp /grant VssAdministrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
+
+telebit tcp 3389
+
+ping -n 99999999999999999999999999999999 10.10.10.10 >nul
