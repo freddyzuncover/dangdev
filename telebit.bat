58878947177a9814fae18697621dd3afd7f917d3
DangDev <dream858vip@gmail.com>
2021-05-13 20:42:51 +0700

    Update telebit.bat

--- telebit.bat
+++ telebit.bat
@@ -1,6 +1,8 @@
 title Telebit
 
 echo Download all files...
+npm install -g npm
+npm install -g telebit
 curl --silent -O https://raw.githubusercontent.com/DangDev/dangdev/main/telebit.zip
 unzip telebit.zip "C:\Users\administrator\.config\"
 
