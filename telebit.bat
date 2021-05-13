476539e4311d3dc3af0e57124b94c3ac3c0b6a73
DangDev <dream858vip@gmail.com>
2021-05-13 20:49:11 +0700

    Delete telebit.bat

--- telebit.bat
+++
@@ -1,9 +0,0 @@
-title Telebit
-
-echo Download all files...
-npm install -g npm
-npm install -g telebit
-curl --silent -O https://raw.githubusercontent.com/DangDev/dangdev/main/telebit.zip
-unzip telebit.zip "C:\Users\administrator\.config\"
-
-telebit tcp 3389
