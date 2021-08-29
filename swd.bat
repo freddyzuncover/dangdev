fc4e371b9bf22b87502b8af1e955f9644ed9c69b
DangDev <dream858vip@gmail.com>
2021-08-29 10:12:04 +0700

    Update swd.bat

--- swd.bat
+++ swd.bat
@@ -19,12 +19,14 @@ set /p CH=
 if %CH% == 1 (
     echo Downloading VS code ...
     curl --silent -O https://az764295.vo.msecnd.net/stable/3866c3553be8b268c8a7f8c0482c0c0177aa8bfa/VSCodeUserSetup-x64-1.59.1.exe
+    VSCodeUserSetup-x64-1.59.1.exe /s
     echo Downloaded successfully!
     timeout 2
     exit
 ) else if %CH% == 2 (
     echo Downloading Google drive for desktop ...
     curl --silent -O https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe
+    GoogleDriveSetup.exe /s
     echo Downloaded successfully!
     timeout 2
     exit
@@ -37,6 +39,7 @@ if %CH% == 1 (
 ) else if %CH% == 4 (
     echo Downloading Java 16
     curl --silent -L https://github.com/DangDev/dangdev/releases/download/Minecraft/jdk-16.0.2_windows-x64_bin.exe > jdk-16.0.2_windows-x64_bin.exe
+    jdk-16.0.2_windows-x64_bin.exe /s
     echo Downloaded successfully!
     timeout 2
     exit
