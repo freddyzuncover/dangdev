395a1d6c05863821d1b6ec7eb1e4580aa452b84c
DangDev <dream858vip@gmail.com>
2021-09-16 20:58:18 +0700

    Update chrome_pipeline.bat

--- chrome_pipeline.bat
+++ chrome_pipeline.bat
@@ -7,20 +7,20 @@ Reg Add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v C
 sc start audiosrv >nul
 
 echo Finished!
-start CONNECT.bat >nul
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"VanDang" > out.txt 2>&1
+net config server /srvcomment:"DiemQuynh" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrator VanDang1 /add >nul
+net user administrator DiemQuynh1 /add >nul
 net localgroup administrators administrator /add >nul
-net user VssAdministrator VanDang1 >nul
-echo User: Administrator
-echo Pass: [Secret]
+net user VssAdministrator DiemQuynh1 >nul
+
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/DangDev/dangdev/main/software/FastConfigVPS_v2.3.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\EVKey Vietnamese Keyboard.exe" https://raw.githubusercontent.com/DangDev/dangdev/main/software/EVKey64.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Enable Session notification.vbs" https://raw.githubusercontent.com/DangDev/dangdev/main/Session.vbs > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\telebit.bat" https://raw.githubusercontent.com/DangDev/dangdev/main/telebit.bat > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\swd.bat" https://raw.githubusercontent.com/DangDev/dangdev/main/swd.bat > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\autoserver.bat" https://raw.githubusercontent.com/DangDev/dangdev/main/dang.bat > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\auto_bedrock.bat" https://raw.githubusercontent.com/DangDev/dangdev/main/auto_bedrock.bat > out.txt 2>&1
 diskperf -Y >nul
 
 sc config Audiosrv start= auto >nul
@@ -28,6 +28,3 @@ ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant VssAdministrator:F >nul
 ICACLS C:\Windows\Temp /grant VssAdministrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
-
-
-ping -n 99999999999999999999999999999999 10.10.10.10 >nul
