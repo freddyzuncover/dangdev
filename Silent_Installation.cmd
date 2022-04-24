3876a6e08aa070dc465280d56fc950662f8195c8
DangDev <dream858vip@gmail.com>
2022-04-24 11:49:03 +0700

    Update Silent_Installation.cmd

--- Silent_Installation.cmd
+++ Silent_Installation.cmd
@@ -15,10 +15,10 @@ echo.
 @echo    //////////////////////////////////////////////////////////////
 @echo off
 curl -L -o Parsec.exe https://builds.parsecgaming.com/package/parsec-windows.exe
-Parsec.exe /VERYSILENT
+Parsec.exe
 curl -L -o Steam.exe https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe
-Steam.exe /VERYSILENT
+Steam.exe
 curl -L -o Ubisoft.exe https://github.com/DangDev/dangdev/releases/download/Apps/UbisoftConnectInstaller.exe
-Ubisoft.exe /VERYSILENT
+Ubisoft.exe
 curl -L -o EpicGames.msi https://github.com/DangDev/dangdev/releases/download/Apps/EpicInstaller-13.3.0.msi
 EpicGames.msi
