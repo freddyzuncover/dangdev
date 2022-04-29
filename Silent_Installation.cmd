c9d492eccaac1c23971fda478f3fa0a377e99a61
DangDev <dream858vip@gmail.com>
2022-04-29 17:58:01 +0700

    Update Silent_Installation.cmd

--- Silent_Installation.cmd
+++ Silent_Installation.cmd
@@ -1,6 +1,8 @@
 @ECHO OFF
 Color 0B
 cd %USERPROFILE%\Desktop
+mkdir HaiDang
+cd %USERPROFILE%\Desktop\HaiDang
 @cls
 echo.
 echo.
@@ -17,11 +19,13 @@ echo.
 @echo off
 curl -L -o Parsec.exe https://builds.parsecgaming.com/package/parsec-windows.exe
 Parsec.exe
-curl -L -o VAC.zip https://download.vb-audio.com/Download_CABLE/VBCABLE_Driver_Pack43.zip
-VAC.exe
 curl -L -o Steam.exe https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe
 Steam.exe
 curl -L -o Ubisoft.exe https://github.com/DangDev/dangdev/releases/download/Apps/UbisoftConnectInstaller.exe
 Ubisoft.exe
 curl -L -o EpicGames.msi https://github.com/DangDev/dangdev/releases/download/Apps/EpicInstaller-13.3.0.msi
 EpicGames.msi
+curl -L -o VAC.zip https://download.vb-audio.com/Download_CABLE/VBCABLE_Driver_Pack43.zip
+tar -xf VAC.zip
+cd %USERPROFILE%\Desktop\HaiDang\VAC
+VBCABLE_Setup_x64.exe
