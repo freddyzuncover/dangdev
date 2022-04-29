7e670dfef4a234ae6708aa0647832707d02d8346
DangDev <dream858vip@gmail.com>
2022-04-29 17:30:32 +0700

    Update Silent_Installation.cmd

--- Silent_Installation.cmd
+++ Silent_Installation.cmd
@@ -1,5 +1,6 @@
 @ECHO OFF
 Color 0B
+cd C:\Users\TinCowBoy\Desktop
 @cls
 echo.
 echo.
@@ -16,7 +17,7 @@ echo.
 @echo off
 curl -L -o Parsec.exe https://builds.parsecgaming.com/package/parsec-windows.exe
 Parsec.exe
-curl -L -o VAC.exe https://github.com/DangDev/dangdev/releases/download/Apps/VACSetup.exe
+curl -L -o VAC.zip https://download.vb-audio.com/Download_CABLE/VBCABLE_Driver_Pack43.zip
 VAC.exe
 curl -L -o Steam.exe https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe
 Steam.exe
